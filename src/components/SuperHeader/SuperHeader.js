417cc56da833825e76016334d3509ffef5405577
nldanne <80318612+nldanne@users.noreply.github.com>
2022-07-20 15:52:08 +0200

    aligned SuperHeader using flexbox

--- src/components/SuperHeader/SuperHeader.js
+++ src/components/SuperHeader/SuperHeader.js
@@ -26,10 +26,15 @@ const Wrapper = styled.div`
   font-size: 0.875rem;
   color: ${COLORS.gray[300]};
   background-color: ${COLORS.gray[900]};
+  padding: 12px 32px;
+  display: flex;
+  gap: 20px;
+  align-items: center;
 `;
 
 const MarketingMessage = styled.span`
   color: ${COLORS.white};
+  margin-right: auto;
 `;
 
 const HelpLink = styled.a`
